9a9c7096b8fd6d48c52017fd5a61f5ec0e5f8488
Scott <103473278+scott12265@users.noreply.github.com>
2022-04-25 18:48:04 +0000

    Changed the visual representation

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function setup(){
   frameRate(60)
   noStroke()
   rectMode(CENTER)
-  createCanvas(2300,600)
-  for(i = 0; i < 1000; i++){
+  createCanvas(5000,600)
+  for(i = 0; i < 2000; i++){
     arr.push(i)
   }
   list = shuffle(arr)
@@ -17,18 +17,11 @@ function draw(){
   background(60)
      for(i = 0; i < list.length; i++){
     fill('green')
-    rect(i*2, height, 1, list[i])
+    rect(i*2, height, 1, list[i]/2)
     if(list[i] < list[i - 1]){
   temp = list[i]
   list[i] = list[i - 1]
   list[i - 1] = temp
-  
     } 
   }
-  console.log('on')
 }
-
- 
-
-
-
